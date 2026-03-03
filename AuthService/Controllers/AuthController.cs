104c8b3f444ee696e295d5bab3e6d93b24da9ad0
Madhavi <Madavi.Ravi@infinite.com>
2026-03-03 10:16:52 +0530

    Initial Auth Service with login endpoint

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using AuthService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login(UserLogin model)
+        {
+            if (model.Username == "Mahesh" && model.Password == "6784")
+                return Ok("Login successful");
+
+            return Unauthorized();
+        }
+    }
+}
