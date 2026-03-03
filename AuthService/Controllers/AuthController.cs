905e71c69b435e6af39bea898f3011dfd480dbb0
Madhavi <Madavi.Ravi@infinite.com>
2026-03-03 12:00:35 +0530

    added authservice

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using AuthService.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly AuthDbContext _context;
+
+        public AuthController(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(Models.UserLogin userLogin)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == userLogin.Username && u.Password == userLogin.Password);
+            if (user == null)
+                return Unauthorized("Invalid credentials!");
+           
+            return Ok("Login successful!");
+        }
+    }
+}
